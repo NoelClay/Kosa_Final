8584cbd1a476faf402d534d396371349cf646468
Kim Namyoung <98522335+NoelClay@users.noreply.github.com>
2024-10-08 18:33:38 +0900

    Add files via upload

---
+++ meta.be-main/UrlBuilder.java
@@ -0,0 +1,34 @@
+package com.dms.datamodelmanagementserver.global;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class UrlBuilder {
+
+//    private static final String BASE_URL1 = "http://localhost:8081"; 
+//    private static final String BASE_URL2 = "http://localhost:8082"; 
+//    private static final String BASE_URL1 = "http://dms:8081"; 
+//    private static final String BASE_URL2 = "http://std:8082"; 
+    private static final String BASE_URL1 = "http://dmstest:8081"; 
+    private static final String BASE_URL2 = "http://stdtest:8082"; 
+
+    public String buildServiceUrl(String path) {
+        String baseUrl;
+
+        if (path.startsWith("/dms")) {
+            baseUrl = BASE_URL1;
+            System.out.println("dmsdmsdsmdsmdsdmsmdsmds :" + path);
+        } else if (path.startsWith("/std")) {
+        	System.out.println("stdstdtsdtsdtsdtsdtstdtsdts :" + path);
+            baseUrl = BASE_URL2;
+        } else {
+            baseUrl = BASE_URL1;
+        }
+
+        return UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .path(path) 
+                .toUriString(); 
+    }
+}
+
