08a5cc7f11a9718a638a1e65421ed04d4f42fab7
NoelClay_gram17 <asdf1578@naver.com>
2024-10-22 16:49:48 +0900

    포스트그레스 디비 생성성

--- DataModelManagementServer/src/main/java/com/dms/datamodelmanagementserver/main/mainController.java
+++ DataModelManagementServer/src/main/java/com/dms/datamodelmanagementserver/main/mainController.java
@@ -3,11 +3,17 @@ package com.dms.datamodelmanagementserver.main;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class mainController {
 	
+
 	@GetMapping(value = "/")
     public String welcomePage() {
+
+        log.info("이거 ㅗ디나?");
         return "redirect:/dms/";
     }
 
